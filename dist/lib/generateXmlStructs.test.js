efb900c99e706e74f24b4114d504f9056f010bd3
Ryan Zheng <nogrethumphrey@gmail.com>
2020-04-08 09:45:04 +0200

    update test file name

--- dist/lib/generateXmlStructs.test.js
+++ dist/lib/generateXmlStructs.test.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const generateXmlStruts_1 = __importDefault(require("./generateXmlStruts"));
-test('adds 1 + 2 to equal 3', () => {
-    generateXmlStruts_1.default('<target folder></target>', '<target file></target>');
-    expect(3).toBe(3);
+test('check xml file generated', () => {
+    generateXmlStruts_1.default('<target folder>', '<target file>');
 });
