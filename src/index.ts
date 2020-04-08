5b6c267127d9a128189b466dc46a4419c7d8e0a6
Ryan Zheng <nogrethumphrey@gmail.com>
2020-04-08 09:25:46 +0200

    check if target folder exist

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import generateXmlForFilesStruts from './lib/generateXmlStruts';
+import fs from 'fs';
 
 if(process.argv.length <= 3) {
     console.log("Usage: " + __filename + " path/to/directory targetXmlFile");
@@ -9,5 +10,10 @@ if(process.argv.length <= 3) {
 let length = process.argv.length;
 let outputPath  = process.argv[length-1];
 let blogsDir = process.argv[length-2];
+
+if (!fs.existsSync(blogsDir)) {
+    console.log(blogsDir + " does not exist");
+    process.exit(1);
+}
 
 generateXmlForFilesStruts(blogsDir,outputPath);
