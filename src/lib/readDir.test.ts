e5c9af9dca107f700e9f139a627f102814a638df
Ryan Zheng <nogrethumphrey@gmail.com>
2020-04-08 08:48:15 +0200

    remove all the files for development

--- src/lib/readDir.test.ts
+++
@@ -1,9 +0,0 @@
-import generateXmlForFilesStruts from './generateXmlStruts';
-
-
-test('adds 1 + 2 to equal 3', () => {
-  generateXmlForFilesStruts(
-    '<target folder></target>',
-    '<target file></target>');
-  expect(3).toBe(3);
-});
